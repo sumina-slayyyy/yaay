719e99ff84a890b6f7c89ac0ab0e4c8f705c6870
Sumi <suminabista365@gmail.com>
2025-12-30 15:49:11 +0545

    Add files via upload

---
+++ operators.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    float b;
+    a = 34;
+    b = 6.45;
+    printf("a + b = %f\n", a+b);
+     printf("a - b = %f\n", a-b);
+      printf("a * b = %f\n", a*b);
+       printf("a / b = %f\n", a/b);
+       return 0;
+} 
